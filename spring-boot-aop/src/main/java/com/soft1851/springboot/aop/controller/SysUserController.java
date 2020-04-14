b49f6a8b496f2c2f149a9c330815bb0012dfce94
wqy <1950016253@qq.com>
2020-04-14 11:15:25 +0800

    权限与鉴权

---
+++ spring-boot-aop/src/main/java/com/soft1851/springboot/aop/controller/SysUserController.java
@@ -0,0 +1,34 @@
+package com.soft1851.springboot.aop.controller;
+
+import com.soft1851.springboot.aop.service.SysUserService;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.Map;
+
+/**
+ * (SysUser)表控制层
+ *
+ * @author wqy
+ */
+@RestController
+@RequestMapping(value = "/sysUser")
+public class SysUserController {
+
+    @Resource
+    private SysUserService sysUserService;
+
+    @PostMapping("/login")
+    public Map<String,Object> signIn(@Param("id") String id,@Param("password") String password) {
+        return sysUserService.signIn(id, password);
+    }
+//
+//    @GetMapping("/g")
+//    public String get() {
+//        System.out.println(111);
+//        return "登录成功";
+//    }
+}
